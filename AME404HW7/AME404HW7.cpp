c0d6b094175de4c4c1ce9dc5fa57e296f7ebf631
mj-pratt <56180494+mj-pratt@users.noreply.github.com>
2021-10-17 22:51:50 -0700

    Initial commit

---
+++ AME404HW7/AME404HW7.cpp
@@ -0,0 +1,12 @@
+
+#include "Vec.h"
+#include <iostream>
+
+
+int main(int num_args, char** args) {
+    double3 a{ 0,1,2 };
+    double3 b{ 0,0,1 };
+    std::cout << a - b << std::endl;
+    std::cout << dot(a, b) << std::endl;
+    return 0;
+}
---
+++ AME404HW7/Vec.h
@@ -0,0 +1,56 @@
+#pragma once
+#ifndef _VEC_H
+#define _VEC_H
+#include <ostream>
+#include <math.h>
+/*! The class used to define 3D vector objects
+@param a maxtrix component i
+@param a maxtrix component j
+@param a maxtrix component k
+
+*/
+
+
+
+
+
+
+
+
+
+
+template<typename T>
+class _vec3 {            // generic 3D vector of type T
+public:
+    _vec3<T>() : d{ 0,0,0 } {}
+    _vec3<T>(T a, T b, T c) : d{ a,b,c } {}
+    T& operator[] (int i) { return d[i]; } 
+    T operator[] (int i) const { return d[i]; }
+
+
+    friend _vec3<T> operator+(const _vec3<T>& a, const _vec3<T>& b)
+    { 
+        return _vec3<T>(a[0] + b[0], a[1] + b[1], a[2] + b[2]);
+    }
+    friend _vec3<T> operator-(const _vec3<T>& a, const _vec3<T>& b)
+    { 
+        return _vec3<T>(a[0] - b[0], a[1] - b[1], a[2] - b[2]);
+    }
+    friend T dot(const _vec3<T>& a, const _vec3<T>& b) {
+        return a[0] * b[0] + a[1] * b[1] + a[2] * b[2];
+    }
+    friend double mag(const _vec3<T>& a) { return sqrt(dot(a, a)); }  
+
+    /*! Returns magnitude \f$ \sqrt{\^{i}^2 + \^{j}^2 + \^{k}^2} \f$
+    @param a vector
+    @return Vector magnitude
+    */
+    friend std::ostream& operator<<(std::ostream& out, const _vec3<T>& a) {
+        out << a[0] << " " << a[1] << " " << a[2]; return out;
+    }
+protected:
+    T d[3]; //!<Underlying array
+};
+using double3 = _vec3<double>;   // assign a nickname 
+#endif
+
